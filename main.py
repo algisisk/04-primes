ad5fd7da0dd98a0961e3b9f4e4c9227ca93f3a7e
algisisk <kylian.algisi-sisowath@edu.esiee.fr>
2024-11-08 14:06:29 +0000

    envoie2

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
+"""
+Ce module contient une fonction pour vérifier si un nombre est premier,
+ainsi qu'une fonction principale pour afficher les nombres premiers jusqu'à 100.
+"""
+
 from math import sqrt
 
-#### Fonction secondaire
-
+# Fonction secondaire
+# La fonction isPrime() permet de renvoyer si le nbr en paramètre est premier
 def isprime(p):
-    premier = True
-    for i in range(2, int(sqrt(p)) + 1):
-        if (p % i == 0):
-            premier = False
-            break
-    return premier
+    """
+    Détermine si un nombre est premier.
     
+    Paramètre:
+    p (int): Le nombre entier à vérifier.
+    
+    Retourne:
+    bool: True si le nombre est premier, False sinon.
+    """
 
-#### Fonction principale
+    if p == 1:
+        return False
+    for i in range(2, int(sqrt(p)) + 1):
+        if p % i == 0:
+            return False
+    return True
 
+# Fonction principale
+# Fonction principale qui fait tourner le code
 def main():
-    # vos appels à la fonction secondaire ici
+    """
+    Affiche tous les nombres premiers de 0 à 99.
+    """
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
