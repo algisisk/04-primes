5cca9ea9402802e5f1951ed036fbe36cc4c989b5
algisisk <kylian.algisi-sisowath@edu.esiee.fr>
2024-11-08 13:29:15 +0000

    envoi1

--- main.py
+++ main.py
@@ -2,26 +2,23 @@ from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
-
-    # votre code ici
-
-    pass
+    premier = True
+    for i in range(2, int(sqrt(p)) + 1):
+        if (p % i == 0):
+            premier = False
+            break
+    return premier
+    
 
 #### Fonction principale
 
-
 def main():
-
     # vos appels à la fonction secondaire ici
-
     for n in range(100):
         if isprime(n):
             print(n, end=", ")
-
     print()
-
 
 if __name__ == "__main__":
     main()
